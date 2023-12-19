e36ddb55a1d1f35b956ba90bd9ea9948b687f6e7
junyue <653566138@qq.com>
2023-12-19 16:16:37 +0800

    init FBLT

---
+++ include/FBLTree.h
@@ -0,0 +1,15 @@
+#if !defined(_FBLTREE_H_)
+#define _FBLTREE_H_
+
+#include "DSM.h"
+#include <atomic>
+#include <city.h>
+#include <functional>
+#include <iostream>
+
+class IndexCache;
+
+
+
+
+#endif //_FBLTREE_H_
